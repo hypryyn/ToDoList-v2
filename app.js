bc4ab99f534ea90ae679d0115d8bb8b103639867
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-04-12 22:51:53 +0700

    adding ejs pages

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["banana", "apple", "watermelon",];
+let workItems = [];
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
@@ -21,15 +22,24 @@ app.get("/", function (req, res) {
 
     var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", { listTitle: day, newListItems: items, route: "/" });
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
+
     let item = req.body.newItem;
     items.push(item);
-
     res.redirect("/");
+});
 
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems, route: "/work" });
+})
+
+app.post("/work", function (req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
 })
 
 app.listen(3000, function () {
