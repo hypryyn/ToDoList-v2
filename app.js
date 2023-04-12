ff81507c3178ee7efc2ccbaf41a6c93b49030e69
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-04-12 21:06:18 +0700

    EJS

--- app.js
+++ app.js
@@ -3,10 +3,34 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req,res){
-    res.send("hello");
+let items = [];
+
+app.use('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function (req, res) {
+
+    var today = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay: day, newListItems: items});
+});
+
+app.post('/', function(req, res){
+    let item = req.body.newItem;
+    items.push(item);
+
+    res.redirect("/");
+
 })
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("server started on port 3000");
-})
+});
