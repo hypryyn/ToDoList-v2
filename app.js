e45084dd0075353fd71e5aa0d23c0b3e5ee004a0
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-04-13 22:18:01 +0700

    refactoring

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["banana", "apple", "watermelon",];
-let workItems = [];
+const items = ["banana", "apple", "watermelon",];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,21 +14,13 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
-    var today = new Date();
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    var day = today.toLocaleDateString("en-US", options);
-
+    const day = date.getDate();
     res.render("list", { listTitle: day, newListItems: items, route: "/" });
 });
 
 app.post("/", function (req, res) {
 
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     items.push(item);
     res.redirect("/");
 });
@@ -37,7 +30,7 @@ app.get("/work", function (req, res) {
 })
 
 app.post("/work", function (req, res) {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     workItems.push(item);
     res.redirect("/work");
 })
